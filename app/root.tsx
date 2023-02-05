ea2bd10c3c7c899edec057b3c6d0c3a15c23d51b
Guillermo Rauch <rauchg@gmail.com>
2023-02-05 08:36:57 -0800

    tweak

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
-import type { LinksFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
 
 import {
   Links,
