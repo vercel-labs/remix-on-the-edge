61e22fc9529a930742c1d23ee21c3b39f8d5496a
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-03-20 17:43:40 -0600

    fix coloring!

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Link, useMatches } from "@remix-run/react";
+import { Link, useMatches, NavLink } from "@remix-run/react";
 
 const SECTION_DATA = [
   { label: "Edge SSR (streaming)", href: "/" },
@@ -9,57 +9,40 @@ const SECTION_DATA = [
 ];
 
 export default function NavigationSwitcher() {
-  const matches = useMatches();
-  const { id } = matches[matches.length - 1];
-  const route = id.replace("routes", "");
-
   return (
     <nav>
       <div className="nav-root">
-        <div aria-hidden className="nav-stroke" />
         <div className="nav-switcher">
           {SECTION_DATA.map((section, i) => {
-            const isActive =
-              section.href === (route === "/index" ? "/" : route);
             return (
-              <Link
-                to={section.href}
-                className={"nav-link" + (isActive ? " active" : "")}
-                key={section.label}
-              >
-                {section.label}
-
-                {isActive ? (
-                  <>
-                    <motion.div
-                      layout
-                      aria-hidden
-                      className="nav-glow"
-                      layoutId="glow"
-                      transition={{
-                        delay: 0.03,
-                        type: "spring",
-                        stiffness: 125,
-                        damping: 20,
-                        mass: 1,
-                      }}
-                    />
-                    <motion.div
-                      layout
-                      initial={false}
-                      aria-hidden
-                      className="nav-pill"
-                      layoutId="pill"
-                      transition={{
-                        type: "spring",
-                        stiffness: 150,
-                        damping: 20,
-                        mass: 1,
-                      }}
-                    />
-                  </>
-                ) : null}
-              </Link>
+                <NavLink
+                  to={section.href}
+                  className={({ isActive, isPending }) => `nav-link ${isActive ? "active" : isPending ? "pending" : ""}`}
+                  key={section.label}
+                >
+                  {({ isActive }) => {
+                    return (
+                      <>
+                        {section.label}
+                        {isActive ? (
+                          <motion.div
+                            layout
+                            initial={false}
+                            aria-hidden
+                            className="nav-pill"
+                            layoutId="pill"
+                            transition={{
+                              type: "spring",
+                              stiffness: 150,
+                              damping: 20,
+                              mass: 1,
+                            }}
+                          />
+                        ) : null}
+                      </>
+                    )
+                  }}
+                </NavLink>
             );
           })}
         </div>
