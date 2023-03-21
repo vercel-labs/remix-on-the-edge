f75b7ed14856a5fa628df61637fe72b01ec51d5b
Nathan Rajlich <n@n8.io>
2023-03-21 12:24:45 -0700

    Remove console.logs()

--- app/parse-vercel-id.ts
+++ app/parse-vercel-id.ts
@@ -1,12 +1,9 @@
 export function parseVercelId(id: string | null) {
-  console.log({ id });
   const parts = id?.split(":").filter(Boolean);
   if (!parts) {
     throw new Error('"x-vercel-id" header not present');
   }
-  console.log({ parts });
   const proxyRegion = parts[0];
   const computeRegion = parts[parts.length - 2];
-  console.log({ proxyRegion, computeRegion });
   return { proxyRegion, computeRegion }
 }
