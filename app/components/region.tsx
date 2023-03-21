e6160ca74400a6e8e1c8b53a8f021be4cb40fb80
Nathan Rajlich <n@n8.io>
2023-03-21 12:31:51 -0700

    .

--- app/components/region.tsx
+++ app/components/region.tsx
@@ -3,8 +3,8 @@ import { regions } from "~/regions";
 export function Region({ region }: { region: string }) {
   const name = regions[region] || "unknown";
   return (
-    <>
+    <span className="region">
       <strong>{name}</strong> <span className="region-code">({region})</span>
-    </>
+    </span>
   );
 }
