3541ed1be7c20d77c3d5832dee7c3b7620b8635b
Nathan Rajlich <n@n8.io>
2023-03-20 23:46:14 -0700

    More nav styling

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,51 +1,77 @@
 import { motion } from "framer-motion";
-import { Link, useMatches, NavLink } from "@remix-run/react";
+import { NavLink, useLocation } from "@remix-run/react";
 
 const SECTION_DATA = [
-  { label: "Edge SSR (streaming)", href: "/" },
-  { label: "Edge SSR", href: "/edge" },
-  { label: "Node.js SSR (streaming)", href: "/node-streaming" },
-  { label: "Node.js SSR", href: "/node" },
+  { label: "Edge (streaming)", href: "/", x: '100%' },
+  { label: "Edge", href: "/edge", x: '62%' },
+  { label: "Node.js (streaming)", href: "/node-streaming", x: '38%' },
+  { label: "Node.js", href: "/node", x: '10%' },
 ];
 
 export default function NavigationSwitcher() {
+  const { pathname } = useLocation();
+  const activeSection = SECTION_DATA.find(section => section.href === pathname);
+
+  const buttons = SECTION_DATA.map((section) => {
+    return (
+      <NavLink
+        to={section.href}
+        className={({ isActive, isPending }) =>
+          `nav-link ${isActive ? "active" : isPending ? "pending" : ""}`
+        }
+        key={section.label}
+      >
+        {({ isActive }) => {
+          return (
+            <>
+              {section.label}
+              {isActive ? (
+                <>
+                  <motion.div
+                    aria-hidden
+                    className="nav-glow"
+                    layoutId="glow"
+                    transition={{
+                      delay: 0.03,
+                      type: "spring",
+                      stiffness: 125,
+                      damping: 20,
+                      mass: 1,
+                    }}
+                  />
+                  <motion.div
+                    aria-hidden
+                    className="nav-pill"
+                    layoutId="pill"
+                    transition={{
+                      type: "spring",
+                      stiffness: 150,
+                      damping: 20,
+                      mass: 1,
+                    }}
+                  />
+                </>
+              ) : null}
+            </>
+          );
+        }}
+      </NavLink>
+    );
+  });
   return (
     <nav>
-      <div className="nav-root">
-        <div className="nav-switcher">
-          {SECTION_DATA.map((section, i) => {
-            return (
-                <NavLink
-                  to={section.href}
-                  className={({ isActive, isPending }) => `nav-link ${isActive ? "active" : isPending ? "pending" : ""}`}
-                  key={section.label}
-                >
-                  {({ isActive }) => {
-                    return (
-                      <>
-                        {section.label}
-                        {isActive ? (
-                          <motion.div
-                            layout
-                            initial={false}
-                            aria-hidden
-                            className="nav-pill"
-                            layoutId="pill"
-                            transition={{
-                              type: "spring",
-                              stiffness: 150,
-                              damping: 20,
-                              mass: 1,
-                            }}
-                          />
-                        ) : null}
-                      </>
-                    )
-                  }}
-                </NavLink>
-            );
-          })}
-        </div>
+      <div
+        className="nav-root"
+        style={
+          activeSection
+            ? {
+                ["--x" as string]: activeSection.x,
+              }
+            : undefined
+        }
+      >
+        <div aria-hidden className="nav-stroke" />
+        <div className="nav-switcher">{buttons}</div>
       </div>
     </nav>
   );
