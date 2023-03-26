72e31554e6031750006a9a962d4606f71a894a2b
Matthias Kühnle <kuehnle@online.de>
2023-03-27 00:18:29 +0200

    parseVercelId to make it executable on localhost (#17)
    Changed throw new Error to console.log in order to be able to execute it on localhost. 
    Returns the values  { proxyRegion: "localhost", computeRegion:"localhost" }

--- app/parse-vercel-id.ts
+++ app/parse-vercel-id.ts
@@ -1,7 +1,8 @@
 export function parseVercelId(id: string | null) {
   const parts = id?.split(":").filter(Boolean);
   if (!parts) {
-    throw new Error('"x-vercel-id" header not present');
+    console.log('"x-vercel-id" header not present. Running on localhost?');
+    return { proxyRegion: "localhost", computeRegion:"localhost" }
   }
   const proxyRegion = parts[0];
   const computeRegion = parts[parts.length - 2];
